b787cbedfa32414053f85485e0be5d319000e162
andreiprepelita <andrei_prepelita@yahoo.com>
2024-01-11 15:11:56 +0200

    Build logic to follow the OAuth1.0 flow for getting Discogs Tokens

--- Final Deliverables Solution S/RecommendationAPI/RecommendationAPI/src/main/java/org/example/RecommendationAPI/util/UtilDiscogs.java
+++ Final Deliverables Solution S/RecommendationAPI/RecommendationAPI/src/main/java/org/example/RecommendationAPI/util/UtilDiscogs.java
@@ -12,9 +12,9 @@ public class UtilDiscogs {
     public static final String ALL_COLLECTIONS_API = "https://api.discogs.com/users/%s/collection/folders";
     public static final String IDENTITY_API = "https://api.discogs.com/oauth/identity";
     public static final String COLLECTION_ITEMS_API = "https://api.discogs.com/users/%s/collection/folders/%d/releases";
-    private static final String TOKEN_URL = "https://api.discogs.com/oauth/request_token";
-    private static final String AUTHENTICATION_URL = "https://www.discogs.com/oauth/authorize";
-    private static final String ACCESS_TOKEN_URL = "https://api.discogs.com/oauth/access_token";
+    public static final String REQUEST_TOKEN_URL = "https://api.discogs.com/oauth/request_token";
+    public static final String AUTHORIZE_URL = "https://www.discogs.com/oauth/authorize";
+    public static final String ACCESS_TOKEN_URL = "https://api.discogs.com/oauth/access_token";
 
 
 
